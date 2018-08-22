8009cc3ce133e64573326dfc2bcba9a5cf3d2e99
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-08-22 15:46:55 +0100

    Adding countdown script

---
+++ countdown.sh
@@ -0,0 +1,13 @@
+secs=$((1 * 60))
+echo "------------------------"
+echo "$1 Counting down from $secs"
+hostname
+uname -a
+pwd
+ls -la
+echo "------------------------"
+while [ $secs -gt 0 ]; do
+   echo -e "$secs"
+   sleep 1
+   : $((secs--))
+done
